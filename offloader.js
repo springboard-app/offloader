c1b55fcee457f97099afa3c667dfc620c25e2746
Aanand Kainth <akainth015@gmail.com>
2019-06-30 04:19:01 -0700

    klasdjf;

--- offloader.js
+++ offloader.js
@@ -11,13 +11,10 @@ admin.initializeApp({
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
 
-db.collection("jobs").onSnapshot(snapshot => {
+db.collection("jobs").onSnapshot(async snapshot => {
     const jobs = snapshot.docs.map(doc => doc.data());
 
-    for (job of jobs.filter(it => !it.assigned && !it.completed)) {
-
-    }
-    jobs.filter(it => !it.assigned && !it.completed).forEach(async (job) => {
+    for (let job of jobs.filter(it => !it.assigned && !it.completed)) {
         const projectId = job.projectId;
         const [contents] = await bucket.file(projectId).download();
         const snapshot = await db.doc(`projects/${projectId}`).get();
@@ -26,19 +23,18 @@ db.collection("jobs").onSnapshot(snapshot => {
         await db.doc(`projects/${projectId}`).update({
             assigned: true
         });
-        console.debug(snapshot.data());
-        exec(`python train_model.py ${projectId} ${trainingPercent / 100} "${type}"`, async (error, stdout, stderr) => {
-            if (error) {
-                console.error(stderr);
-                await db.doc(`projects/${projectId}`).update({
-                    completed: true,
-                    errored: true
-                });
+        fs.writeFile(`data_matrix.csv`, contents, async err => {
+            if (err) {
+                console.error(err);
                 return;
             }
-            fs.writeFile(`data_matrix.csv`, contents, async err => {
-                if (err) {
-                    console.error(err);
+            exec(`python train_model.py ${projectId} ${trainingPercent / 100} "${type}"`, async (error, stdout, stderr) => {
+                if (error) {
+                    console.error(stderr);
+                    await db.doc(`projects/${projectId}`).update({
+                        completed: true,
+                        errored: true
+                    });
                     return;
                 }
                 let path = `${projectId}.joblib`;
@@ -53,5 +49,5 @@ db.collection("jobs").onSnapshot(snapshot => {
                 });
             });
         });
-    });
+    }
 });
